e55ae1f35f9fe04d8e28c04d069e0d8df1619b6b
Freek Michielsen <freek.mich@gmail.com>
2023-06-26 14:52:55 +0200

    Add band buffer and max option

--- AudioVisualiser/Assets/Scripts/AudioScript.cs
+++ AudioVisualiser/Assets/Scripts/AudioScript.cs
@@ -6,11 +6,21 @@ using System;
 public class AudioScript : MonoBehaviour
 {
     private AudioSource source;
-    [SerializeField]private BandMixScriptableObject bandMix;
-    
+    [SerializeField] private BandMixScriptableObject bandMix;
+    [SerializeField] private bool useBandMaxes = false;
+    [SerializeField] private bool useBuffer = true;
+
+
+    [SerializeField] private float bufferDecreaseBase = 0.005f;
+    [SerializeField] private float bufferDecreaseScale = 1.2f;
+
     private float[] spectrum;
 
     private int[] bandBorders;
+    private float[] bands;
+
+    private float[] bandBuffers;
+    private float[] bufferDecreases;
 
     public static event Action<float[]> OnAudioUpdate;
 
@@ -31,18 +41,26 @@ public class AudioScript : MonoBehaviour
         for (int i = 0; i < bandBorders.Length; i++)
         {
             bandBorders[i] = (int)(bandMix.InputBandRanges[i] * songFrequencyStep);
-            Debug.Log($"step{i}: {bandBorders[i]}");
         }
+
+        bandBuffers = new float[bandBorders.Length + 1];
+        bufferDecreases = new float[bandBorders.Length + 1];
     }
-    
+
 
     private void Update()
     {
-        source.GetSpectrumData(spectrum,0 ,FFTWindow.Blackman);
-        OnAudioUpdate?.Invoke(GetAverages());
-    }    
+        source.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
 
-    private float[] GetAverages()
+        if (useBandMaxes) bands = GetBandMaximums();
+        else bands = GetBandAverages();
+
+        SetBandBuffer();
+        if (useBuffer) bands = bandBuffers;
+        OnAudioUpdate?.Invoke(bands);
+    }
+
+    private float[] GetBandAverages()
     {
         float[] averages = new float[bandBorders.Length + 1];
         averages[0] = GetBandAverage(0, bandBorders[0]);
@@ -52,7 +70,7 @@ public class AudioScript : MonoBehaviour
             averages[i] = GetBandAverage(bandBorders[i - 1], bandBorders[i]);
         }
 
-        averages[0] = GetBandAverage(bandBorders[bandBorders.Length -1] , bandMix.SpectrumRange);
+        averages[0] = GetBandAverage(bandBorders[bandBorders.Length - 1], bandMix.SpectrumRange);
         return averages;
     }
 
@@ -60,9 +78,6 @@ public class AudioScript : MonoBehaviour
     {
         float average = 0;
         int count = 0;
-
-        Debug.Log("1 " + pBandBorder1);
-        Debug.Log("2 " + pBandBorder2);
 
         for (int i = pBandBorder1; i < pBandBorder2; i++)
         {
@@ -72,5 +87,50 @@ public class AudioScript : MonoBehaviour
 
         average = average / count;
         return average;
+    }
+
+    private float[] GetBandMaximums()
+    {
+        float[] maximums = new float[bandBorders.Length + 1];
+        maximums[0] = GetBandMax(0, bandBorders[0]);
+
+        for (int i = 1; i < bandBorders.Length; i++)
+        {
+            maximums[i] = GetBandMax(bandBorders[i - 1], bandBorders[i]);
+        }
+
+        maximums[0] = GetBandMax(bandBorders[bandBorders.Length - 1], bandMix.SpectrumRange);
+        return maximums;
+    }
+
+
+    private float GetBandMax(int pBandBorder1, int pBandBorder2)
+    {
+        float max = 0;
+
+        for (int i = pBandBorder1; i < pBandBorder2; i++)
+        {
+            if (spectrum[i] > max) max = spectrum[i];
+        }
+        return max;
+    }
+
+
+    private void SetBandBuffer()
+    {
+        for (int i = 0; i < bands.Length; i++)
+        {
+            if (bands[i] > bandBuffers[i])
+            {
+                bandBuffers[i] = bands[i];
+                bufferDecreases[i] = bufferDecreaseBase;
+            }
+            else
+            {
+                bandBuffers[i] -= bufferDecreases[i] * Time.deltaTime;
+                bufferDecreases[i] *=  1 + (bufferDecreaseScale * Time.deltaTime);
+                
+            }
+        }
     }
 }
